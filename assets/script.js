22b9dd8ecd875a162ed93747608e144fff7bdc47
pat paggi <patpaggi@gmail.com>
2020-10-11 09:53:41 -0700

    started

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+
+
+var startQuiz = document.querySelector("begin");
+var highScore = document.querySelector("scores");
+
+
+var beginQuiz = function () {
+  // add timer
+  // remove start btn.
+
+  /*question1 ... add ul. 
+  li.. for each questions
+  */
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THis will start quiz
+
+startQuiz.addEventListener("click", beginQuiz);
