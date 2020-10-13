b17220b526cb5a4ec297a08cb7a2ee690e00dc65
pat paggi <patpaggi@gmail.com>
2020-10-12 23:15:36 -0700

    fix timer

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,18 @@
 "use strict";
-var questions = [{
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "a"
-},
-{
-  question: "What does HTLM stand for?",
-  answers: ["Hyper Text Markup Language ", "Hyper Text Link Markerup", "Some computer lanquage", "Hotel Californa"],
-  correct: "a"
-}, {
-  question: "What does DOM stand for?",
-  answers: ["a", "b ", "c", "d"],
-  correct: "b"
-}, {
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "d"
-},
-{
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "a"
-}];
 
 var startQuiz = document.querySelector("#begin");
 var highScore = document.querySelector("#scores");
 var quiz = document.querySelector("main");
 var timeEl = document.querySelector("#time")
 var aBtn = document.createElement("button");
-aBtn.id = "aButton";
+aBtn.value = aBtn.innerText;
 var bBtn = document.createElement("button");
-bBtn.id = "bButton";
+bBtn.value = bBtn.innerText;
 var cBtn = document.createElement("button");
-cBtn.id = "cButton";
+cBtn.value = cBtn.innerText;
 var dBtn = document.createElement("button");
-dBtn.id = "dButton";
+dBtn.value = dBtn.innerText;
 
-var buttonA = document.getElementById("aButton");
-var buttonB = document.getElementById("bButton");
-var buttonC = document.getElementById("cButton");
-var buttonD = document.getElementById("dButton");
 
 var currentQuestion = 0;
 var score = 0;
@@ -85,16 +58,25 @@ function showQuestion() {
   quiz.appendChild(answerC);
   quiz.appendChild(answerD);
 
-  buttonA.addEventListener("click", showNextQuestion);
-  buttonB.addEventListener("click", showNextQuestion);
-  buttonC.addEventListener("click", showNextQuestion);
-  buttonD.addEventListener("click", showNextQuestion);
+  var buttonA = document.getElementById("aBtn");
+  var buttonB = document.getElementById("bBtn");
+  var buttonC = document.getElementById("cBtn");
+  var buttonD = document.getElementById("dBtn");
+
+  aBtn.addEventListener("click", showNextQuestion, aBtn);
+  bBtn.addEventListener("click", showNextQuestion, bBtn);
+  cBtn.addEventListener("click", showNextQuestion, cBtn);
+  dBtn.addEventListener("click", showNextQuestion, dBtn);
+
+
 
 }
 /* click event
 check answer fucntion
 */
 var showNextQuestion = function () {
+  quiz.innerHTML = "";
+
   currentQuestion = currentQuestion + 1
   //Question
   var questionPage = document.createElement("ul");
@@ -109,17 +91,14 @@ var showNextQuestion = function () {
   answerA.appendChild(aBtn);
 
   var answerB = document.createElement("li");
-
   bBtn.textContent = questions[currentQuestion].answers[1];
   answerB.appendChild(bBtn);
 
   var answerC = document.createElement("li");
-
   cBtn.textContent = questions[currentQuestion].answers[2];
   answerC.appendChild(cBtn);
 
   var answerD = document.createElement("li");
-
   dBtn.textContent = questions[currentQuestion].answers[3];
   answerD.appendChild(dBtn);
 
@@ -132,10 +111,11 @@ var showNextQuestion = function () {
 
 
 
-  buttonA.addEventListener("click", showNextQuestion);
-  buttonB.addEventListener("click", showNextQuestion);
-  buttonC.addEventListener("click", showNextQuestion);
-  buttonD.addEventListener("click", showNextQuestion);
+
+  aBtn.addEventListener("click", showNextQuestion, aBtn);
+  bBtn.addEventListener("click", showNextQuestion, bBtn);
+  cBtn.addEventListener("click", showNextQuestion, cBtn);
+  dBtn.addEventListener("click", showNextQuestion, dBtn);
 
   /*
   questions ...add ul.
@@ -156,7 +136,7 @@ startQuiz.addEventListener("click", beginQuiz);
 // add timer TIMER goes into -
 function stopWatch() {
   // If the count down is finished, write some text 
-  var timeInterval = setInterval(function () {
+  var timerInterval = setInterval(function () {
     timer--;
     timeEl.textContent = timer;
     if (timer <= 0) {
@@ -166,3 +146,27 @@ function stopWatch() {
     }
   }, 1000);
 }
+
+var questions = [{
+  question: "Question",
+  answers: ["a", "b", "c", "d"],
+  correct: "a"
+},
+{
+  question: "What does HTLM stand for?",
+  answers: ["Hyper Text Markup Language ", "Hyper Text Link Markerup", "Some computer lanquage", "Hotel Californa"],
+  correct: "a"
+}, {
+  question: "What does DOM stand for?",
+  answers: ["a", "b ", "c", "d"],
+  correct: "b"
+}, {
+  question: "Question",
+  answers: ["a", "b", "c", "d"],
+  correct: "d"
+},
+{
+  question: "Question",
+  answers: ["a", "b", "c", "d"],
+  correct: "a"
+}];
