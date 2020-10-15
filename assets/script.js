b9cac268e1ebc86e5997308ed59de2d58efe2032
pat paggi <patpaggi@gmail.com>
2020-10-15 16:37:35 -0700

    local storage done

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var startQuiz = document.querySelector("#begin");
-var highScore = document.querySelector("#scores");
+var highScore = document.querySelector("#dropdown");
+//var playersScore = localStorage.setItem(playersScore)
 var quiz = document.querySelector("main");
 var timeEl = document.querySelector("#time")
 var aBtn = document.createElement("button");
@@ -12,15 +13,20 @@ var cBtn = document.createElement("button");
 cBtn.value = cBtn.innerText;
 var dBtn = document.createElement("button");
 dBtn.value = dBtn.innerText;
-
+var highScoreBtn = document.createElement("button");
+highScoreBtn.type = ("submit")
+highScoreBtn.id = "high"
+highScoreBtn.innerText = "Submit your initials."
 
 var currentQuestion = 0;
 var score = 0;
 var timer = 60
 
 
+
 var beginQuiz = function () {
   // remove start btn.
+  score = 0;
   quiz.innerHTML = "";
   stopWatch();
   showQuestion();
@@ -89,18 +95,71 @@ var endGame = function () {
   endPage.innerText = "Thank you for taking the quiz";
   var yourScore = document.createElement("h2");
   yourScore.innerText = "Your is" + " " + score + "!";
-  var highScoreBtn = document.createElement("button");
-  highScoreBtn.type = ("submit")
+  var initialsInput = document.createElement("input");
+  initialsInput.id = "initials"
+  var form = document.createElement("form");
 
+
+
+  // use local storage
   quiz.appendChild(endPage);
   quiz.appendChild(yourScore);
+  quiz.appendChild(form);
+  form.appendChild(initialsInput);
   quiz.appendChild(highScoreBtn);
 
+
+  highScoreBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    var initialL = document.querySelector("#initials")
+    console.log(initialL)
+    var initials = initialL.value.trim();
+    console.log(initials);
+    localStorage.setItem("score", score)
+    localStorage.setItem("initials", initials)
+    addHighScore();
+  })
   /*
-  Create end page and start over button
-  create input field for high score 
-  */
+  need to use localStorage  and start over button
+*/
+
+
 }
+var addHighScore = function () {
+
+
+  var newPerson = document.createElement("li");
+  newPerson.innerText = "i did it";
+
+  highScore.appendChild(newPerson)
+}
+/*
+var playersScore = []
+playersScore = [{
+  id: 1,
+  name: "Add localStorage persistence",
+  player: initials,
+  finalScore: score
+},
+{
+  id: 2,
+  name: "Add localStorage persistence",
+  player: initials,
+  finalScore: score
+}
+]
+// create new array to hold updated list of highscore
+var updatedplayersScoreArr = [];
+
+// loop through currentplayersScore
+for (var i = 0; i < playersScore.length; i++) {
+  ifplayersScore[i].id
+  //doesn't match the value of playersScoreId, let's keep that playersScore and push it into the new array
+  if (playersScores[i].id !== parseInt(playersScoreId)) {
+    updatedplayersScoreArr.push(playersScores[i]);
+  }
+}
+*/
 
 
 var showNextQuestion = function () {
@@ -108,43 +167,48 @@ var showNextQuestion = function () {
   quiz.innerHTML = "";
   currentQuestion = currentQuestion + 1
   //Question
-  var questionPage = document.createElement("ul");
-  questionPage.className = "question";
-  questionPage.innerText = questions[currentQuestion].question;
+  if (currentQuestion < questions.length) {
+    var questionPage = document.createElement("ul");
+    questionPage.className = "question";
+    questionPage.innerText = questions[currentQuestion].question;
 
-  //Answers
-  var answerA = document.createElement("li");
-  var aBtn = document.createElement("button");
-  aBtn.id = "aButton";
-  aBtn.innerText = questions[currentQuestion].answers[0];
-  answerA.appendChild(aBtn);
+    //Answers
+    var answerA = document.createElement("li");
+    var aBtn = document.createElement("button");
+    aBtn.id = "aButton";
+    aBtn.innerText = questions[currentQuestion].answers[0];
+    answerA.appendChild(aBtn);
 
-  var answerB = document.createElement("li");
-  bBtn.textContent = questions[currentQuestion].answers[1];
-  answerB.appendChild(bBtn);
+    var answerB = document.createElement("li");
+    bBtn.textContent = questions[currentQuestion].answers[1];
+    answerB.appendChild(bBtn);
 
-  var answerC = document.createElement("li");
-  cBtn.textContent = questions[currentQuestion].answers[2];
-  answerC.appendChild(cBtn);
+    var answerC = document.createElement("li");
+    cBtn.textContent = questions[currentQuestion].answers[2];
+    answerC.appendChild(cBtn);
 
-  var answerD = document.createElement("li");
-  dBtn.textContent = questions[currentQuestion].answers[3];
-  answerD.appendChild(dBtn);
-
-
-  quiz.appendChild(questionPage)
-  quiz.appendChild(answerA);
-  quiz.appendChild(answerB);
-  quiz.appendChild(answerC);
-  quiz.appendChild(answerD);
+    var answerD = document.createElement("li");
+    dBtn.textContent = questions[currentQuestion].answers[3];
+    answerD.appendChild(dBtn);
 
 
+    quiz.appendChild(questionPage)
+    quiz.appendChild(answerA);
+    quiz.appendChild(answerB);
+    quiz.appendChild(answerC);
+    quiz.appendChild(answerD);
 
 
-  aBtn.addEventListener("click", checkAnswer,);
-  bBtn.addEventListener("click", checkAnswer,);
-  cBtn.addEventListener("click", checkAnswer,);
-  dBtn.addEventListener("click", checkAnswer,);
+
+
+    aBtn.addEventListener("click", checkAnswer,);
+    bBtn.addEventListener("click", checkAnswer,);
+    cBtn.addEventListener("click", checkAnswer,);
+    dBtn.addEventListener("click", checkAnswer,);
+  }
+  else {
+    endGame()
+  }
 
   /*
   questions ...add ul.
@@ -175,25 +239,47 @@ function stopWatch() {
 }
 
 var questions = [{
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "a"
+  question: "What would be the largest font?",
+  answers: ["h2", "a", "p", "h6"],
+  correct: "h2"
 },
 {
-  question: "What does HTLM stand for?",
-  answers: ["Hyper Text Markup Language ", "Hyper Text Link Markerup", "Some computer lanquage", "Hotel Californa"],
-  correct: "a"
+  question: "What does HTML stand for?",
+  answers: ["Hyper Text Markup Language", "Hyper Text Markerup Link", "Some computer lanquage", "Hotel Californa"],
+  correct: "Hyper Text Markup Language"
 }, {
   question: "What does DOM stand for?",
-  answers: ["a", "b ", "c", "d"],
-  correct: "b"
+  answers: ["Domain Over Model", "Document Object Model", "Domain of Main", "Developers Over Rated"],
+  correct: "Document Object Model"
 }, {
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "d"
+  question: "How do you close a tag in HTML?",
+  answers: ["/", ";", "/>", "</"],
+  correct: "</"
 },
 {
-  question: "Question",
-  answers: ["a", "b", "c", "d"],
-  correct: "a"
-}];
+  question: "< div Class='show' id='cake'> In this div what is the way you link the style to it in CSS?",
+  answers: ["show", ".div", ".show", ".cake"],
+  correct: ".show"
+},
+{
+  //more questions
+  question: "What would be the largest font?",
+  answers: ["h2", "a", "p", "h6"],
+  correct: "h2"
+},
+{
+  question: "What would be the largest font?",
+  answers: ["h2", "a", "p", "h6"],
+  correct: "h2"
+},
+{
+  question: "What would be the largest font?",
+  answers: ["h2", "a", "p", "h6"],
+  correct: "h2"
+},
+{
+  question: "What would be the largest font?",
+  answers: ["h2", "a", "p", "h6"],
+  correct: "h2"
+}]
+  ;
